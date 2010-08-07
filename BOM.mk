1dbfacc1750fc605fae08156f990eacf5100e39a
Joseph Wayne Norton <norton@alum.mit.edu>
2010-08-07 14:51:16 +0900

    Update README, Makefile, BOM.mk, and GMBOM files for building with bom.sh

--- BOM.mk
+++ BOM.mk
@@ -1,43 +1,41 @@
 ###----------------------------------------------------------------------
 ### Copyright (c) 2007-2010 Gemini Mobile Technologies, Inc.  All rights reserved.
-###
+### 
 ### Licensed under the Apache License, Version 2.0 (the "License");
 ### you may not use this file except in compliance with the License.
 ### You may obtain a copy of the License at
-###
+### 
 ###     http://www.apache.org/licenses/LICENSE-2.0
-###
+### 
 ### Unless required by applicable law or agreed to in writing, software
 ### distributed under the License is distributed on an "AS IS" BASIS,
 ### WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 ### See the License for the specific language governing permissions and
 ### limitations under the License.
 ###
-### File    : Makefile
-### Purpose : 
+### File    : BOM.mk
+### Purpose :
 ###----------------------------------------------------------------------
 
-MODULES = \
-	tbf \
-	tbf_driver \
-	thrift_contract_parser \
-	ubf_thrift_plugin
+ifeq ($(erl-bom-mk),)
+include $(SRCDIR)/$(firstword $(filter src/erl-tools/gmt-bom__%,$(MY_DEPENDS)))/make/erl_bom.mk
+endif
 
-PLUGINS = \
-	ubf_thrift_plugin
+$(ME)/.bom_config: $(erl-bom-mk)
+	$(erl-bom-config)
+	touch $@
 
-EUNITTESTS = \
-	thrift_plugin_app \
-	thrift_plugin_sup \
-	thrift_plugin_test
+$(ME)/.bom_build: $(ME)/.bom_config $(erl-bom-mk)
+	$(erl-bom-build)
+	touch $@
 
-# include erl make
-include $(shell cat ../.depends-gmt-bom)/make/erl_make.mk
+$(ME)/.bom_install: $(ME)/.bom_build $(erl-bom-mk)
+	$(erl-bom-install)
+	touch $@
 
-# standard targets
-check: all
-	-rm -f ./*.log
-	$(RUNERL1) -noinput -noshell -pz $(EUNITTEST_DIR) \
-		-s thrift_plugin_test do_eunit \
-		-s erlang halt \
-		> ./check.log
+$(ME)/.bom_test: $(ME)/.bom_install $(erl-bom-mk)
+	$(erl-bom-test)
+
+$(ME)/.bom_clean: $(erl-bom-mk)
+	$(erl-bom-clean)
+
