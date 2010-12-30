5d691cd065107a4dbe9e8ca26ef9cf5c58056ed8
Joseph Wayne Norton <norton@alum.mit.edu>
2010-12-30 23:15:55 +0900

    Misc. dialyzer warning fixes

--- src/thrift_contract_parser.erl
+++ src/thrift_contract_parser.erl
@@ -12,24 +12,24 @@
 %%====================================================================
 
 parse_transform(In, _Opts) ->
-    %% io:format("In:~p~n   Opts: ~p~n",[In, _Opts]),
-    [M] = [X || {attribute, _, module, X} <- In],
-    Name = atom_to_list(M),
-    VSN = case [X || {attribute, _, vsn, X} <- In] of [V] -> V; _ -> "" end,
-    Imports = [X || {attribute, _, add_types, X} <- In],
-    case [X || {attribute, _, add_contract, X} <- In] of
-	[File] ->
-	    case file(Name, VSN, Imports, File ++ infileExtension()) of
-		{ok, Contract, _Header} ->
-		    %% io:format("Contract added: ~p~n", [Contract]),
-		    contract_parser:parse_transform_contract(In, Contract);
-		{error, Why} ->
-		    io:format("Error in contract:~p~n", [Why]),
-		    erlang:error(Why)
-	    end;
-	[] ->
-	    In
-    end.
+    %% %% io:format("In:~p~n   Opts: ~p~n",[In, _Opts]),
+    %% [M] = [X || {attribute, _, module, X} <- In],
+    %% Name = atom_to_list(M),
+    %% VSN = case [X || {attribute, _, vsn, X} <- In] of [V] -> V; _ -> "" end,
+    %% Imports = [X || {attribute, _, add_types, X} <- In],
+    %% case [X || {attribute, _, add_contract, X} <- In] of
+	%% [File] ->
+	%%     case file(Name, VSN, Imports, File ++ infileExtension()) of
+	%% 	{ok, Contract, _Header} ->
+	%% 	    %% io:format("Contract added: ~p~n", [Contract]),
+	%% 	    contract_parser:parse_transform_contract(In, Contract);
+	%% 	{error, Why} ->
+	%% 	    io:format("Error in contract:~p~n", [Why]),
+	%% 	    erlang:error(Why)
+	%%     end;
+	%% [] ->
+    In.
+    %%end.
 
 %%====================================================================
 %% External API
@@ -40,21 +40,21 @@ parse_transform(In, _Opts) ->
 %% Internal functions
 %%====================================================================
 
-infileExtension()  -> ".thrift".
+%% infileExtension()  -> ".thrift".
 
-file(_Name, _VSN, _Imports, F) ->
-    %% io:format("~n~p ~p~n~p~n~p~n", [Name, VSN, Imports, F]),
-    case file:read_file(F) of
-        {ok, _Bin} ->
-            exit(notimplemented);
-            %% case thriftc:parse(binary_to_list(Bin), []) of
-            %%     {ok, AST, _Rest} ->
-            %%         Types = ast2ubf(thriftc_ast:ast_to_int_form(AST)),
-            %%         %% io:format("~n~p~n", [Types]),
-            %%         contract_parser:tags([{name,Name}, {vsn,VSN}, {types,Types}], Imports);
-            %%     Err ->
-            %%         Err
-            %% end;
-        Err ->
-            Err
-    end.
+%% file(_Name, _VSN, _Imports, F) ->
+%%     %% io:format("~n~p ~p~n~p~n~p~n", [Name, VSN, Imports, F]),
+%%     case file:read_file(F) of
+%%         {ok, _Bin} ->
+%%             exit(notimplemented);
+%%             %% case thriftc:parse(binary_to_list(Bin), []) of
+%%             %%     {ok, AST, _Rest} ->
+%%             %%         Types = ast2ubf(thriftc_ast:ast_to_int_form(AST)),
+%%             %%         %% io:format("~n~p~n", [Types]),
+%%             %%         contract_parser:tags([{name,Name}, {vsn,VSN}, {types,Types}], Imports);
+%%             %%     Err ->
+%%             %%         Err
+%%             %% end;
+%%         Err ->
+%%             Err
+%%     end.
